86d5fb0d91d5028eaff28cf96d46bac55d8e9317
Маргарита Бережная <rita.berezhnaya.2005@mail.ru>
2025-10-02 00:45:55 +0700

    Очистка счетчика

--- app.py
+++ app.py
@@ -4,13 +4,17 @@ app = Flask(__name__)
 
 @app.errorhandler(404)
 def not_found(err):
-    return "нет такой страницы", 404
+    return "Нет такой страницы", 404
+
 @app.route("/")
 @app.route("/lab1")
 def lab1():
     return """<!doctype html>
         <html>
             <body>
+            <head>
+                <link rel="stylesheet" href="""" + url_for('static', filename='lab1.css') + """">
+            </head>
                 <ul>
                     <h1>Лабораторная работа 1</h1>
                     <li><a href="/author">author</a></li>
@@ -26,16 +30,15 @@ def web():
     return """<!doctype html>
         <html>
             <body>
+            <head>
+                <link rel="stylesheet" href="""" + url_for('static', filename='lab1.css') + """">
+            </head>
                 <h1>web-сервер на flask</h1>
                 <a href="/author">author</a>
                 <br>
                 <a href="/lab1">Назад к главной</a>
             </body>
-        
-         </html>""", 200, {
-            'X-Server': 'sample',
-            'Content-Type': 'text/plain; charset=utf-8'
-        }
+        </html>"""
 
 @app.route("/author")
 def author():
@@ -45,6 +48,9 @@ def author():
     
     return """<!doctype html>
         <html>
+            <head>
+                <link rel="stylesheet" href="""" + url_for('static', filename='lab1.css') + """">
+            </head>
             <body>
                 <p>Студент: """ + name + """</p>
                 <p>Группа: """ + group + """</p>
@@ -59,12 +65,16 @@ def author():
 @app.route('/image') 
 def image():
     path = url_for("static", filename="oak.jpg")
-    return '''
+    css_url = url_for('static', filename='lab1.css')
+    return f'''
 <!doctype html>
 <html>
+    <head>
+        <link rel="stylesheet" href="{css_url}">
+    </head>
     <body>
         <h1>Дуб</h1>
-        <img src="''' + path + '''" width="800">
+        <img src="{path}">
         <br>
         <a href="/lab1">Назад к главной</a>
     </body>
@@ -83,26 +93,44 @@ def counter():
     return '''
 <!doctype html>
 <html>
+    <head>
+        <link rel="stylesheet" href="''' + url_for('static', filename='lab1.css') + '''">
+    </head>
     <body>
+        <h2>Счетчик посещений</h2>
         Сколько раз вы сюда заходили: ''' + str(count) + '''
         <hr>
         Дата и время: ''' + str(time) + '''<br>
         Запрошенный адрес: ''' + url + '''<br>
         Ваш IP-адрес: ''' + client_ip + '''<br>
+        <a href="/lab1">Назад к главной</a>
+        <br>
+        <a href="/reset_counter">Очистить счётчик</a>
     </body>
 </html>
 '''
+@app.route('/reset_counter')
+def reset_counter():
+    global count
+    count = 0
+    return redirect('/counter')
+    
 @app.route("/info")
 def info():    
-        return redirect("/author")
+    return redirect("/author")
+
 @app.route("/created")
 def created():
     return '''
 <!doctype html>
 <html>
+    <head>
+        <link rel="stylesheet" href="''' + url_for('static', filename='lab1.css') + '''">
+    </head>
     <body>
         <h1>Создано успешно</h1>
         <div><i>что-то создано...</i></div>
+        <a href="/lab1">Назад к главной</a>
     </body>
 </html>
 ''', 201
