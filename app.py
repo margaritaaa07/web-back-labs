875ad1880c48198fec624b8f81ac72e9760bf2b0
Маргарита Бережная <rita.berezhnaya.2005@mail.ru>
2025-10-02 01:15:26 +0700

    Коды ответов HTTP

--- app.py
+++ app.py
@@ -6,6 +6,104 @@ app = Flask(__name__)
 def not_found(err):
     return "Нет такой страницы", 404
 
+# ← УБЕРИТЕ ОТСТУП ЗДЕСЬ! Эта строка должна быть на одном уровне с @app.errorhandler
+@app.route("/bad_request")
+def bad_request():
+    return '''
+<!doctype html>
+<html>
+    <head>
+        <title>400 Bad Request</title>
+        <link rel="stylesheet" href="''' + url_for('static', filename='lab1.css') + '''">
+    </head>
+    <body>
+        <h1>400 Bad Request</h1>
+        <p>Сервер не может обработать запрос из-за некорректного синтаксиса.</p>
+        <a href="/">На главную</a>
+    </body>
+</html>''', 400
+
+@app.route("/unauthorized")
+def unauthorized():
+    return '''
+<!doctype html>
+<html>
+    <head>
+        <title>401 Unauthorized</title>
+        <link rel="stylesheet" href="''' + url_for('static', filename='lab1.css') + '''">
+    </head>
+    <body>
+        <h1>401 Unauthorized</h1>
+        <p>Требуется аутентификация для доступа к ресурсу.</p>
+        <a href="/">На главную</a>
+    </body>
+</html>''', 401
+
+@app.route("/payment_required")
+def payment_required():
+    return '''
+<!doctype html>
+<html>
+    <head>
+        <title>402 Payment Required</title>
+        <link rel="stylesheet" href="''' + url_for('static', filename='lab1.css') + '''">
+    </head>
+    <body>
+        <h1>402 Payment Required</h1>
+        <p>Зарезервировано для будущего использования. Первоначально предназначалось для цифровых платежных систем.</p>
+        <a href="/">На главную</a>
+    </body>
+</html>''', 402
+
+@app.route("/forbidden")
+def forbidden():
+    return '''
+<!doctype html>
+<html>
+    <head>
+        <title>403 Forbidden</title>
+        <link rel="stylesheet" href="''' + url_for('static', filename='lab1.css') + '''">
+    </head>
+    <body>
+        <h1>403 Forbidden</h1>
+        <p>Доступ к запрошенному ресурсу запрещен.</p>
+        <a href="/">На главную</a>
+    </body>
+</html>''', 403
+
+@app.route("/method_not_allowed")
+def method_not_allowed():
+    return '''
+<!doctype html>
+<html>
+    <head>
+        <title>405 Method Not Allowed</title>
+        <link rel="stylesheet" href="''' + url_for('static', filename='lab1.css') + '''">
+    </head>
+    <body>
+        <h1>405 Method Not Allowed</h1>
+        <p>Метод запроса не поддерживается для данного ресурса.</p>
+        <a href="/">На главную</a>
+    </body>
+</html>''', 405
+
+@app.route("/teapot")
+def teapot():
+    return '''
+<!doctype html>
+<html>
+    <head>
+        <title>418 I'm a teapot</title>
+        <link rel="stylesheet" href="''' + url_for('static', filename='lab1.css') + '''">
+    </head>
+    <body>
+        <h1>418 I'm a teapot</h1>
+        <p>Я - чайник. Не могу заварить кофе.</p>
+        <a href="/">На главную</a>
+    </body>
+</html>''', 418
+
+
 @app.route("/")
 @app.route("/index")
 def index():
@@ -55,13 +153,37 @@ def lab1():
         
         <a href="/">На главную</a>
         <ul>
-            <li><a href="/lab1/author">author</a></li>
-            <li><a href="/lab1/web">web</a></li>
-            <li><a href="/lab1/image">image</a></li>
-            <li><a href="/lab1/counter">counter</a></li>
+            <li><a href="/lab1/author">Автор</a></li>
+            <li><a href="/lab1/web">WEB</a></li>
+            <li><a href="/lab1/image">Дуб</a></li>
+            <li><a href="/lab1/counter">Счетчик</a></li>
+            <li><a href="/http_codes">Коды ответов HTTP</a></li>
         </ul>
     </body>
 </html>''' 
+
+@app.route("/http_codes")
+def http_codes():
+    return '''
+<!doctype html>
+<html>
+    <head>
+        <link rel="stylesheet" href="''' + url_for('static', filename='lab1.css') + '''">
+        <title>Коды ответов HTTP</title>
+    </head>
+    <body>
+        <h1>Коды ответов HTTP</h1>
+        <ul>
+            <li><a href="/bad_request">400 - Bad Request</a></li>
+            <li><a href="/unauthorized">401 - Unauthorized</a></li>
+            <li><a href="/payment_required">402 - Payment Required</a></li>
+            <li><a href="/forbidden">403 - Forbidden</a></li>
+            <li><a href="/method_not_allowed">405 - Method Not Allowed</a></li>
+            <li><a href="/teapot">418 - I'm a teapot</a></li>
+        </ul>
+        <a href="/">На главную</a>
+    </body>
+</html>'''
 
 @app.route("/lab1/web")  
 def web():
