a217299a6ebbe6140dbf7b5ea683714c8932c530
Маргарита Бережная <rita.berezhnaya.2005@mail.ru>
2025-10-02 00:17:18 +0700

    код ответа 201

--- app.py
+++ app.py
@@ -87,7 +87,18 @@ def counter():
 '''
 @app.route("/info")
 def info():    
-    return redirect("/author")
+        return redirect("/author")
+@app.route("/created")
+def created():
+    return '''
+<!doctype html>
+<html>
+    <body>
+        <h1>Создано успешно</h1>
+        <div><i>что-то создано...</i></div>
+    </body>
+</html>
+''', 201
 
 if __name__ == '__main__':
     app.run(debug=True)
