fd81627e13ee64b2ebe7cc79f6add7aa2671878d
Маргарита Бережная <rita.berezhnaya.2005@mail.ru>
2025-10-03 19:55:23 +0700

    Добавление ссылок

--- app.py
+++ app.py
@@ -345,7 +345,8 @@ def index():
         <main>
             <nav>
                 <ul>
-                     <li><a href="/lab1">Первая лабораторная</a></li>
+                    <li><a href="/lab1">Первая лабораторная</a></li>
+                     <li><a href="/lab2">Вторая лабораторная</a></li>
                 </ul>
              </nav>
         </main>
