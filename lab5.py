2f00dc60323f77bcc3319f0625a861f5e6bade57
Маргарита Бережная <rita.berezhnaya.2005@mail.ru>
2025-11-20 18:35:26 +0700

    Исправление SQL-инъекций

--- lab5.py
+++ lab5.py
@@ -187,7 +187,7 @@ def register():
 
         print(f" Добавляем пользователя: {login}")
         password_hash = generate_password_hash(password)
-        cur.execute("INSERT INTO users (login, password) VALUES (%s, %s)", (login, password_hash))
+        cur.execute("INSERT INTO users (login, password) VALUES (%s, %s);", (login, password_hash))
         conn.commit()
 
         print(f" Пользователь {login} добавлен в БД")
@@ -219,7 +219,7 @@ def login():
     try:
         conn, cur = db_connect()
         
-        cur.execute("SELECT * FROM users WHERE login = %s", (login,))
+        cur.execute("SELECT * FROM users WHERE login=%s;", (login, ))
         user = cur.fetchone()
         
         if user and check_password_hash(user['password'], password):
@@ -263,7 +263,7 @@ def create():
     try:
         conn, cur = db_connect()
 
-        cur.execute("SELECT id FROM users WHERE login = %s", (login,))
+        cur.execute(f"SELECT id FROM users WHERE login = %s;", (login,))
         user = cur.fetchone()
         
         if not user:
